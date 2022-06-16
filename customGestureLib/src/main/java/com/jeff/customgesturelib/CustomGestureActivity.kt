a73c045ea58dff75fa83791c7340d1e7e656abaf
JeffLiu <jeff.liu@nectec.com.tw>
2022-06-16 16:03:48 +0800

    Add CustomGestureActivity

---
+++ customGestureLib/src/main/java/com/jeff/customgesturelib/CustomGestureActivity.kt
@@ -0,0 +1,11 @@
+package com.jeff.customgesturelib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomGestureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gesture)
+    }
+}
